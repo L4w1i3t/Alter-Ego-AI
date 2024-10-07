04566896974c2e22dfdb84a9cbac619c0de07d3e
L4w1i3t <84912726+L4w1i3t@users.noreply.github.com>
2024-10-07 14:45:37 -0400

    Rolled back on python gui

--- electron/renderer/preload.js
+++
@@ -1,3 +0,0 @@
-// electron/renderer/preload.js
-// Preload script can be used to expose limited Node.js functionality to the renderer
-// For enhanced security, consider using contextBridge and IPC channels
